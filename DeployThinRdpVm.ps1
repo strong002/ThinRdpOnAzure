3468f5e1af09d96e5b470f2c013ad1bc2b70065b
strong002 <43149922+strong002@users.noreply.github.com>
2020-10-20 11:06:57 -0400

    Update DeployThinRdpVm.ps1

--- DeployThinRdpVm.ps1
+++ DeployThinRdpVm.ps1
@@ -1,5 +1,5 @@
 # using Azure PowerShell 0.9.8
 Switch-AzureMode AzureResourceManager
 $group = "ThinRdpVm"
-New-AzureResourceGroup -Name $group -Location "West Europe"
+New-AzureResourceGroup -Name $group -Location "USGov Virginia"
 New-AzureResourceGroupDeployment -ResourceGroupName $group -TemplateFile ".\ThinRdpVm.json" -TemplateParameterFile ".\ThinRdpVm.param.json" 
